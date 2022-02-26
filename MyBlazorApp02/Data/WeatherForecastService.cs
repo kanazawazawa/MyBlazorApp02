6ebd30562fb1513b50a8f68efecefd73154dbbfb
kanazawazawa <you@example.com>
2022-02-26 21:28:24 +0900

    hello world

--- MyBlazorApp02/Data/WeatherForecastService.cs
+++ MyBlazorApp02/Data/WeatherForecastService.cs
@@ -4,7 +4,7 @@ namespace MyBlazorApp02.Data
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching‚ "
     };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
